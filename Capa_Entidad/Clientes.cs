505394c4da24f84c87d51f9fd2671dcea5199e69
Charlichg <charlie.chinchilla.godinez@gmail.com>
2023-06-02 13:20:31 -0600

    cambio

--- Capa_Entidad/Clientes.cs
+++ Capa_Entidad/Clientes.cs
@@ -4,6 +4,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.ComponentModel.DataAnnotations;
 
 namespace Capa_Entidad
 {
@@ -12,15 +13,33 @@ namespace Capa_Entidad
         public string Codigo { get; set; }
         public string Num_Identificacion { get; set; }
         public int Tipo_Identificacion { get; set; }
+
+        [Required(AllowEmptyStrings = false, ErrorMessage = "El nombre es requerido")]
         public string Nombre { get; set; }
+
+        [Required(AllowEmptyStrings = false, ErrorMessage = "El Primer apellido es requerido")]
         public string Primer_Apellido { get; set; }
+
+        [Required(AllowEmptyStrings = false, ErrorMessage = "El Segundo apellido es requerido")]
         public string Segundo_Apellido { get; set; }
+
+        [Required(AllowEmptyStrings = false, ErrorMessage = "La Direccion es requerida")]
         public string Direccion { get; set; }
+
+
         public int Perfil_Transaccional { get; set; }
+
+        [Required(AllowEmptyStrings = false, ErrorMessage = "El Pais Es Requerido")]
         public int Pais { get; set; }
+
+        [Required(AllowEmptyStrings = false, ErrorMessage = "El Estado_Civil Es Requerido")]
         public int Estado_Civil { get; set; }
+
+        [Required(AllowEmptyStrings = false, ErrorMessage = "La Profesion Es Requerida")]
         public string Profesion { get; set; }
+
         public string Lugar_Trabajo { get; set; }
+
         public int Tipo_Cliente { get; set; }
 
         // Propiedades de navegación

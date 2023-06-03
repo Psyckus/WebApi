4c71ab80a6faa5ae7e7348d1296815ec19df6f01
Charlichg <charlie.chinchilla.godinez@gmail.com>
2023-06-03 13:07:41 -0600

    cambios validacion

--- Capa_Entidad/Clientes.cs
+++ Capa_Entidad/Clientes.cs
@@ -20,28 +20,28 @@ namespace Capa_Entidad
         public string Nombre { get; set; }
 
 
-        [MaxLength(2, ErrorMessage = "El apellido debe ser de dos caracteres")]
+       // [MaxLength(2, ErrorMessage = "El apellido debe ser de dos caracteres")]
         public string Primer_Apellido { get; set; }
 
         //[Required(ErrorMessage = "El Segundo Apellido Es Obligatorio.")]
         public string Segundo_Apellido { get; set; }
-
-       // [Required(ErrorMessage = "La Direcion Es Obligatoria.")]
+     
+        [RegularExpression("^[a-zA-Z]+$", ErrorMessage = " La Direccion solo debe contener letras.")]
         public string Direccion { get; set; }
 
-
+        [Required(ErrorMessage = "El Perfil_Transaccional Es Obligatorio.")]
         public string Perfil_Transaccional { get; set; }
 
-        //[Required(ErrorMessage = "El Nombre del Pais Es Obligatorio.")]
+        [Required(ErrorMessage = "El Nombre del Pais Es Obligatorio.")]
         public string Pais { get; set; }
 
-       // [Required(ErrorMessage = "El Estado_Civil Es Obligatorio.")]
+        [Required(ErrorMessage = "El Estado_Civil Es Obligatorio.")]
         public string Estado_Civil { get; set; }
 
-       // [Required(ErrorMessage = "La Profesion Es Obligatoria.")]
+        [Required(ErrorMessage = "La Profesion Es Obligatoria.")]
         public string Profesion { get; set; }
 
-        //[Required(ErrorMessage = "El Lugar De Trabajo Es Obligatorio.")]
+        [Required(ErrorMessage = "El Lugar De Trabajo Es Obligatorio.")]
         public string Lugar_Trabajo { get; set; }
 
         public string Tipo_Cliente { get; set; }
